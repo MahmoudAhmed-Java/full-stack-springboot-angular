56669418b1a78478c6bc596ec8c4f712d1fb46d2
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2026-01-11 14:03:35 +0200

    eCommerce Project - Search for Products By Category

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.luv2code.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -10,4 +13,5 @@ import com.luv2code.ecommerce.entity.Product;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,25 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: false,
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
+  //templateUrl: './product-list-table.component.html',
+  //templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
 
   products:Product[] = [];
+  currentCategoryId: number = 1;
 
-  constructor(private productedService: ProductService){}
+  constructor(private productedService: ProductService, private route: ActivatedRoute){}
 
   ngOnInit(): void{
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
+    
   }
 
   listProducts() {
-    this.productedService.getProductList().subscribe(
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if(hasCategoryId){
+        this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }else{
+      this.currentCategoryId = 1;
+    }
+    this.productedService.getProductList(this.currentCategoryId).subscribe(
       data => { this.products = data; }
     );
   }
