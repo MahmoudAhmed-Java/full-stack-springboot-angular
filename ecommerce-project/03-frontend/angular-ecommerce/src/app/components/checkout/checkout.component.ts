43418c2840574f8e98da0b5dad87e1f504d88534
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2026-01-14 11:53:07 +0200

    eCommerce Project - Checkout Form Layout

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  standalone: false,
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.css'
+})
+export class CheckoutComponent {
+
+  checkoutFormGroup!: FormGroup;
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+  
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
+      }),
+    });
+  }
+
+  // copyShippingAddressToBillingAddress($event: Event) {
+  //   if(event?.target.checked){
+  //     this.checkoutFormGroup.controls.billingAddress
+  //         .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+  //   }else{
+  //     this.checkoutFormGroup.controls.billingAddress.reset();
+  //   }
+  // }
+
+  copyShippingAddressToBillingAddress(event: Event) {
+
+  const checkbox = event.target as HTMLInputElement;
+
+  if (checkbox.checked) {
+    this.checkoutFormGroup.get('billingAddress')?.setValue(
+      this.checkoutFormGroup.get('shippingAddress')?.value
+    );
+  } else {
+    this.checkoutFormGroup.get('billingAddress')?.reset();
+  }
+}
+
+
+  onSubmit() {
+    console.log("Handling the submit button");
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log("The email address is: " + this.checkoutFormGroup.get('customer')?.value.email);
+  }
+
+}
