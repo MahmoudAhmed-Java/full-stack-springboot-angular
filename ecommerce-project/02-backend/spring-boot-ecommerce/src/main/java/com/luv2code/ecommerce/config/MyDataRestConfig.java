b311ad0c537af8d3534134a45ed87a7b4c2d2e7f
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2026-01-17 12:45:47 +0200

    eCommerce Project - Checkout - Form  - Drop - Down Lists

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -1,73 +1,76 @@
 package com.luv2code.ecommerce.config;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.core.mapping.ConfigurableHttpMethods;
-import org.springframework.data.rest.core.mapping.ExposureConfigurer.AggregateResourceHttpMethodsFilter;
-import org.springframework.data.rest.core.mapping.ResourceMetadata;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
+import com.luv2code.ecommerce.entity.Country;
 import com.luv2code.ecommerce.entity.Product;
 import com.luv2code.ecommerce.entity.ProductCategory;
+import com.luv2code.ecommerce.entity.State;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-	private EntityManager entityManager;
-	
-	@Autowired
-	public MyDataRestConfig(EntityManager entityManager) {
-		this.entityManager = entityManager;
-	}
-	
-	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-		final HttpMethod[] theUnSupportedActions = {HttpMethod.DELETE , HttpMethod.PUT , HttpMethod.POST};
-		config.getExposureConfiguration().forDomainType(Product.class)
-		.withItemExposure( new AggregateResourceHttpMethodsFilter() {
-			public ConfigurableHttpMethods filter(ResourceMetadata metdata, ConfigurableHttpMethods httpMethods) {
-				return httpMethods.disable(theUnSupportedActions);
-			}
-		})
-		.withCollectionExposure( new AggregateResourceHttpMethodsFilter() {
-			public ConfigurableHttpMethods filter(ResourceMetadata metdata, ConfigurableHttpMethods httpMethods) {
-				return httpMethods.disable(theUnSupportedActions);
-			}
-		} );
-		
-		config.getExposureConfiguration().forDomainType(ProductCategory.class)
-		.withItemExposure( new AggregateResourceHttpMethodsFilter() {
-			public ConfigurableHttpMethods filter(ResourceMetadata metdata, ConfigurableHttpMethods httpMethods) {
-				return httpMethods.disable(theUnSupportedActions);
-			}
-		})
-		.withCollectionExposure( new AggregateResourceHttpMethodsFilter() {
-			public ConfigurableHttpMethods filter(ResourceMetadata metdata, ConfigurableHttpMethods httpMethods) {
-				return httpMethods.disable(theUnSupportedActions);
-			}
-		} );
-		
-		exposeIds(config);
-	}
+    private final EntityManager entityManager;
 
-	private void exposeIds(RepositoryRestConfiguration config) {
-		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
-		List<Class> entityClasses = new ArrayList<Class>();
-		for( EntityType tempEntityType : entities) {
-			entityClasses.add(tempEntityType.getJavaType());
-		}
-		Class[] domainTypes = entityClasses.toArray(new Class[0]);
-		config.exposeIdsFor(domainTypes);
-	}
-	
-	
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(
+            RepositoryRestConfiguration config,
+            CorsRegistry cors) {
+
+        HttpMethod[] unsupportedActions = {
+                HttpMethod.PUT,
+                HttpMethod.POST,
+                HttpMethod.DELETE
+        };
+
+        disableHttpMethods(Product.class, config, unsupportedActions);
+        disableHttpMethods(ProductCategory.class, config, unsupportedActions);
+        disableHttpMethods(Country.class, config, unsupportedActions);
+        disableHttpMethods(State.class, config, unsupportedActions);
+
+        exposeIds(config);
+    }
+
+    private void disableHttpMethods(
+            Class<?> domainClass,
+            RepositoryRestConfiguration config,
+            HttpMethod[] unsupportedActions) {
+
+        config.getExposureConfiguration()
+                .forDomainType(domainClass)
+                .withItemExposure((metadata, httpMethods) ->
+                        httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) ->
+                        httpMethods.disable(unsupportedActions));
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class<?>> entityClasses = new ArrayList<>();
+
+        for (EntityType<?> entityType : entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        Class<?>[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
 }
--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Luv2ShopFormService } from '../../services/luv2-shop-form.service';
+import { State } from '../../common/state';
+import { Country } from '../../common/country';
 
 @Component({
   selector: 'app-checkout',
@@ -12,9 +15,17 @@ export class CheckoutComponent {
   checkoutFormGroup!: FormGroup;
   totalPrice: number = 0;
   totalQuantity: number = 0;
-  
 
-  constructor(private formBuilder: FormBuilder) { }
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  countries: Country[] = [];
+  states: State[] = [];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
+  constructor(private formBuilder: FormBuilder, private luv2ShopFormService: Luv2ShopFormService) { }
 
   ngOnInit() {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -46,6 +57,28 @@ export class CheckoutComponent {
         expirationYear: ['']
       }),
     });
+
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("startMonth: "+startMonth);
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data
+      }
+    );
+    this.luv2ShopFormService.getCreditCardYears().subscribe(
+      data => {
+        console.log("Retrieved credit card years: " + JSON.stringify(data));
+        this.creditCardYears = data;
+      }
+    );
+
+    this.luv2ShopFormService.getCountries().subscribe(
+      data => {
+        console.log("Retrieved countries: "+JSON.stringify(data));
+        this.countries = data;
+      }
+    );
   }
 
   // copyShippingAddressToBillingAddress($event: Event) {
@@ -65,8 +98,10 @@ export class CheckoutComponent {
     this.checkoutFormGroup.get('billingAddress')?.setValue(
       this.checkoutFormGroup.get('shippingAddress')?.value
     );
+    this.billingAddressStates = this.shippingAddressStates;
   } else {
     this.checkoutFormGroup.get('billingAddress')?.reset();
+    this.billingAddressStates = [];
   }
 }
 
@@ -75,6 +110,44 @@ export class CheckoutComponent {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log("The email address is: " + this.checkoutFormGroup.get('customer')?.value.email);
+    console.log("The shipping address country is: " + this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
+    console.log("The shipping address state is: " + this.checkoutFormGroup.get('shippingAddress')?.value.state.name);
+  }
+
+  handleMonthsAndYears(){
+    const creditCardFormGroup = this.checkoutFormGroup.get('crediCard');
+    const currentYear: number = new Date().getFullYear();
+    const selectYear: number = Number(creditCardFormGroup?.value.expirationYear);
+    let startMonth: number;
+    if(currentYear === selectYear ){
+      startMonth = new Date().getMonth() + 1;
+    }else{
+      startMonth = 1;
+    }
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+  }
+
+  getStates(formGroupName: string){
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+    this.luv2ShopFormService.getStates(countryCode).subscribe(
+      data => {
+        if( formGroupName === 'shippingAddress'){
+          this.shippingAddressStates = data;
+        }else {
+          this.billingAddressStates = data;
+        }
+        formGroup!.get('state')!.setValue(data[0]);
+      }
+    );
   }
 
 }
